8d16e561ceacb8d702fbc5fab5dc057baa6a1128
Bill Li <billli@gmail.com>
2021-11-20 16:48:53 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    // <div>
+    //   comment this out!
+    // </div>
+    <Router>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route exact path="/">
+                  <Homepage />
+                </Route>
+                <Route exact path="/exchanges">
+                  <Exchanges />
+                </Route>
+                <Route exact path="/cryptocurrencies">
+                  <Cryptocurrencies />
+                </Route>
+                <Route exact path="/crypto/:coinId">
+                  <CryptoDetails />
+                </Route>
+                <Route exact path="/news">
+                  <News />
+                </Route>
+              </Routes>
+            </div>
+          </Layout>
+        </div>
+        <div className="footer">
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Exchanges = () => {
+  return (
+    <div>
+      hi there
+    </div>
+  );
+}
+
+export default Exchanges;
